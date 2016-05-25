f4b80f28a0bfbd6aedad39feb1041798bf8a769e
Craig Zilles <zilles@illinois.edu>
2016-05-25 16:40:30 -0500

    documentation of content.js and extension of README

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # articulation
 Chrome extension for efficiently handling UIUC articulation forms
 
+Installation
+------------
+
+  1. Clone or download the repository.  
+  2. Configure (see below).  
+  3. Open Chrome browser
+  4. go to:  chrome://extensions  (or Tools > Extensions under the Chrome menu)
+  5. Click Load unpacked extension... to pop up a file-selection dialog.
+  6. Navigate to the directory in which this extension's files live, and select it.
+
+
+Configuration
+-------------
+
 Currently configured for Computer Science courses, but could be configured for other departments/units.  Configuration is currently performed by editing the popup.js file and adding/modifying entries in the _classes array.  (Sorry...)
 
 Currently, it supports two kinds of entries:
@@ -14,3 +28,18 @@ Currently, it supports two kinds of entries:
      `'CS 101' : { rubric: 'CS', number: '101', isrealclass: true, hours: 3, title: 'Intro. to Computing for Science and Engineering' },`
 
 In both cases, the first thing (the key) is the thing that will show up in the popup list.
+
+
+
+Usage
+-----
+
+If the articulation page is not already the active tab, clicking this
+extension will open a new tab and load the articulation page.
+
+If the articulation page is the active tab, clicking the extension
+will bring up a menu.  Selecting a course from the menu will attempt
+to populate the articulation page.  After the page is filled in, the
+user can make any manual changes (e.g., write an explanation) and then
+submit, either using the normal means in the form or by using the
+'Submit' button from this extension.
--- content.js
+++ content.js
@@ -1,3 +1,20 @@
+/**
+ * Below is the sequence of DOM elements to try to edit, in the order they should be edited by the extension.
+ * 
+ * The form is interactive, based on your selections, it presents different options.  This is why the order
+ * matters (at least in some cases).  Also, it explains why (below) we use 'setTimeout' as a wait mechanism
+ * for the DOM elements to be created.
+ * 
+ * We handle the fact that not all DOM elements will exist in two ways:
+ *   1. 'ignoreIfNotPresent' will cause us to skip the update if the DOM element isn't present and 
+ *      if the desired field is not present in the classinfo object received from the extension
+ *   2. 'tryonce' will cause us to skip this update if the DOM element isn't present.  This is a bit
+ *      of a hack to avoid having to have separate names for 'rubric' for the two different kinds of 
+ *      objects.  It is important that a 'tryonce' update is not the first update following any action
+ *      that updates the DOM.  (Otherwise, we can't distinguish a DOM element that hasn't loaded yet,
+ *      from one that isn't ever going to get loaded.)
+ */
+
 var steps = [
   { id: 'ctl00_ContentPlaceHolder1_rdo_purview_0', action: 'click' },
   { id: 'ctl00_ContentPlaceHolder1_txt_program', action: 'text', field: 'rubric' },
@@ -5,20 +22,30 @@ var steps = [
   { id: 'ctl00_ContentPlaceHolder1_rdo_multicourse_sequence_1', action: 'conditional', field: 'isrealclass', condition: false },
   { id: 'ctl00_ContentPlaceHolder1_rdo_multicourse_sequence_0', action: 'conditional', field: 'isrealclass', condition: true },
   { id: 'ctl00_ContentPlaceHolder1_txt_non_equiv_level',  action: 'text', field: 'level', ignoreIfNotPresent: true },
+  // 'rubric' follows 'level' because level will ensure that it is loaded, so we can use 'tryonce'
   { id: 'ctl00_ContentPlaceHolder1_txt_non_equiv_rubric', action: 'text', field: 'rubric', tryonce: true },
   { id: 'ctl00_ContentPlaceHolder1_txt_course_number',  action: 'text', field: 'number', ignoreIfNotPresent: true },
   { id: 'ctl00_ContentPlaceHolder1_txt_credit_hours',  action: 'text', field: 'hours', ignoreIfNotPresent: true },
   { id: 'ctl00_ContentPlaceHolder1_txt_course_title',  action: 'text', field: 'title', ignoreIfNotPresent: true },
+  // 'rubric' follows 'number' because number will ensure that it is loaded, so we can use 'tryonce'
   { id: 'ctl00_ContentPlaceHolder1_txt_rubric',  action: 'text', field: 'rubric', tryonce: true },
 ];
 
-
 // not eligible text box 'ctl00_ContentPlaceHolder1_txt_suggestions'
 
+
+/**
+ * Perform a single step in filling out the form, using 'steps' above
+ *
+ * @param {object} classinfo - information about the class to use for populating the form
+ * @param {integer} stepNumber - index (starting at 0) in the step array
+ */
 function fillInForm(classinfo, stepNumber) {
-  var step = steps[stepNumber];
-  console.log(step.id);
-  var timeOut = 0;
+  var timeOut = 0;	       		   // time before attempting next step
+  var step = steps[stepNumber];		   // info associated with current step
+  // console.log(step.id);
+
+  // get the indicated DOM element.  If present, perform the specified operation.
   var widget = document.getElementById(step.id);
   if (widget) {
     switch (step.action) {
@@ -31,7 +58,7 @@ function fillInForm(classinfo, stepNumber) {
         }
         break;
       case 'text':
-        console.log(step.field + ": " + classinfo[step.field]);
+        // console.log(step.field + ": " + classinfo[step.field]);
         widget.value = classinfo[step.field];
         break;
       case 'constText':
@@ -54,10 +81,16 @@ function fillInForm(classinfo, stepNumber) {
 }
 
 
-
+/**
+ * Setup a receiver for messages from the extension.
+ *
+ * There are two main kinds of messages.  
+ *  1. those to force a click of the submit button.
+ *  2. those to fill out a form using the information from a classinfo object
+ */
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(request);
+    // console.log(request);
     if (request.submit_only) {     
       document.getElementById('ctl00_ContentPlaceHolder1_btn_submit').click();
     } else {
